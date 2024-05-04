833f059af3a0f3b72287b1d76bc3291b09090fc8
Ludger Solbach <soulman@soulspace.org>
2024-05-04 23:18:30 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -6,26 +6,26 @@
   :main org.soulspace.annuity.application
 
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.csv "0.1.4"]
-                 [org.soulspace.clj/clj.swing "0.8.0"]
-                 [org.soulspace.clj/cmp.batik "0.2.1"]
-                 [org.soulspace.clj/cmp.fop "0.3.1"]
+                 [org.clojure/data.csv "1.1.0"]
+                 [org.soulspace.clj/clj.swing "0.8.4-SNAPSHOT"]
+                 [org.soulspace.clj/cmp.poi "0.6.5-SNAPSHOT"]
+                 [org.soulspace.clj/cmp.fop "0.3.3-SNAPSHOT"]
+                 [org.soulspace.clj/cmp.batik "0.2.2-SNAPSHOT"]
                  [org.soulspace.clj/cmp.jfreechart "0.5.0"]
-                 [org.soulspace.clj/cmp.poi "0.6.4"]
                  [org.soulspace.clj/math.financial "0.7.0"]
                  [org.soulspace.clj/xml.core "0.5.1"]
                  [org.soulspace.clj/xml.dsl "0.5.2"]]
 
   :test-paths ["test"]
-  :repl-options {:init-ns org.soulspace.annuity.application}
 
-  
+  :repl-options {:init-ns org.soulspace.annuity.application}
   :profiles {:dev {:dependencies [[djblue/portal "0.49.1"]
                                   [criterium/criterium "0.4.6"]
                                   [com.clojure-goes-fast/clj-java-decompiler "0.3.4"]
                                     ; [expound/expound "0.9.0"]
                                   ]
-                   :global-vars {*warn-on-reflection* true}}}
+                  ; :global-vars {*warn-on-reflection* true}
+                   }}
 
 
 
